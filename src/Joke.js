2e227eaa5095d6914473b0f13cc0b03de6d9da90
JuliaGood <gudak.yuliya@gmail.com>
2021-02-15 20:28:01 +0200

    Styling the Joke Component

--- src/Joke.js
+++ src/Joke.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Joke.css';
 
 class Joke extends Component {
   render() {
@@ -6,11 +7,13 @@ class Joke extends Component {
       <div className='Joke'>
         <div className='Joke-buttons'>
           <i className='fas fa-arrow-up' onClick={this.props.upVote} ></i>
-          <span>{this.props.votes}</span>
+          <span className='Joke-votes'>{this.props.votes}</span>
           <i className='fas fa-arrow-down' onClick={this.props.downVote} ></i>
         </div>
-
         <div className='Joke-text'>{this.props.text}</div>
+        <div className='Joke-smiley'>
+          <i className="em em-sweat_smile"></i>
+        </div>
       </div>
     );   
   }
